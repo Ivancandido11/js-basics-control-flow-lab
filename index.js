425125bfecbdc54dfa7246fef8c00119f2526526
Ivan Candido <ivan.candido@powerhrg.com>
2021-05-20 20:16:01 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400){
+    return "This one is on me!"
+  } else if (someNumber > 2000 && someNumber <= 2500){
+      return "I will gladly take your thirty bucks."
+  } else if (someNumber > 2500){
+      return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const cityMessage = city === 'NYC' ? "Ok, sounds good." : "No go.";
+  return cityMessage;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let tipMessage;
+  switch (tip){
+    case 'generous':
+      tipMessage = "Thank you so much.";
+      break;
+    case 'not as generous':
+      tipMessage = "Thank you.";
+      break;
+    case 'thanks for everything':
+      tipMessage = "Bye."
+      break;
+  }
+  return tipMessage;
 }
